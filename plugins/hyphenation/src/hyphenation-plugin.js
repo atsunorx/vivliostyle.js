cda3b716862f53032a62ca85fede6a3fa5adc4fa
unageanu <masaya.yamauchi@gmail.com>
2016-10-27 09:13:31 +0900

    Add plugin template for hyphenation.
    - Create plugins/hyphenation directory.
    - Add hypher.js to dependencies.
    - Add `copy:hyphenation_dictionaries` task that collect hyphenation dictionaries.

---
+++ plugins/hyphenation/src/hyphenation-plugin.js
@@ -0,0 +1,12 @@
+
+/**
+ * Copyright 2016 Vivliostyle Inc.
+ * @fileoverview Hyphenation plugin
+ */
+goog.provide("plugins.hyphenation");
+require("node_modules/hypher/lib/hypher");
+
+goog.require("vivliostyle.plugin");
+
+goog.scope(function() {
+});
