ef976c853defe0e4c5b559addba8d71b020b8d77
KAWAKUBO Toru <kwkbtr@vivliostyle.com>
2016-12-15 10:44:38 +0900

    Fix typo

--- plugins/hyphenation/test/spec/forbid-hyphenation-at-the-end-of-columns-text-node-breaker-spec.js
+++ plugins/hyphenation/test/spec/forbid-hyphenation-at-the-end-of-columns-text-node-breaker-spec.js
@@ -30,30 +30,30 @@ describe("vivliostyle.plugins.hyphenation", function() {
                 expect(newContext.offsetInNode).toEqual(17);
                 expect(textNode.replaceData).not.toHaveBeenCalled();
             });
-            it("removes characters character after the word boundary before the soft hyphen when splits a text node at the soft-hyphen character.", function() {
+            it("removes characters after the word boundary before the soft hyphen when splits a text node at the soft-hyphen character.", function() {
                 var newContext = breaker.breakTextNode(textNode, nodeContext, 13, checkPoints, 1);
                 expect(newContext.offsetInNode).toEqual(5);
                 expect(textNode.replaceData).toHaveBeenCalledWith(4, 13, '');
             });
-            it("removes characters character after the word boundary before the hyphen when splits a text node at the hyphen character.", function() {
+            it("removes characters after the word boundary before the hyphen when splits a text node at the hyphen character.", function() {
                 textNode.data = 'abcd abcde\u00ADf gh-j';
                 var newContext = breaker.breakTextNode(textNode, nodeContext, 18, checkPoints, 1);
                 expect(newContext.offsetInNode).toEqual(13);
                 expect(textNode.replaceData).toHaveBeenCalledWith(12, 5, '');
             });
-            it("returns null when splits a text node at the soft hyphen character in the first line.", function() {
+            it("returns null when splits a text node at the soft hyphen character in the first word.", function() {
                 textNode.data = 'abcdeabcde\u00ADf gh\u00ADj';
                 var newContext = breaker.breakTextNode(textNode, nodeContext, 13, checkPoints, 1);
                 expect(newContext).toEqual(null);
                 expect(textNode.replaceData).not.toHaveBeenCalled();
             });
-            it("returns null when splits a text node at the soft hyphen character in the first line.", function() {
+            it("returns null when splits a text node at the hyphen character in the first word.", function() {
                 textNode.data = 'abcdeabcde-f gh-j';
                 var newContext = breaker.breakTextNode(textNode, nodeContext, 13, checkPoints, 1);
                 expect(newContext).toEqual(null);
                 expect(textNode.replaceData).not.toHaveBeenCalled();
             });
-            it("removes characters after split point and inserts a hyphenation character when splits a text node at the space character.", function() {
+            it("removes characters after split point when splits a text node at the space character.", function() {
                 var newContext = breaker.breakTextNode(textNode, nodeContext, 15, checkPoints, 1);
                 expect(newContext.offsetInNode).toEqual(13);
                 expect(textNode.replaceData).toHaveBeenCalledWith(13, 4, '');
